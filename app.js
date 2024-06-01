8840142b15e8cdcbe157e35b1ca0ffd9b8c5c2e6
Thareeq Roshan <troshan@microsoft.com>
2024-06-01 21:44:53 +0200

    Added sound selection and tap tempo prototype

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const startStopButton = document.querySelector(".start-stop");
 const subtractBeatsButton = document.querySelector(".subtract-beats");
 const addBeatsButton = document.querySelector(".add-beats");
 const measureCount = document.querySelector(".measure-count");
+const soundSelector01 = document.querySelector(".sound-selector-01");
+const soundSelector02 = document.querySelector(".sound-selector-02");
+const tapTempoButton = document.querySelector(".tap-tempo-button");
 
 const clickLower = new Audio(
   "https://github.com/thareeqroshan/JSMetronome/raw/main/click_lower.mp3"
@@ -19,13 +22,39 @@ const clickUpper = new Audio(
 );
 clickLower.preload = "auto";
 
+let clicks = [clickLower, clickUpper];
+
 let bpm = 140;
 let beatsPerMeasure = 4;
 let tempoTextString = "Medium";
 let isRunning = false;
 let count = 0;
+let selectedSound = 1;
+let timeSinceLastTap = 0;
+soundSelector01.classList.add("selected");
 const metronome = new Timer(playClick, 60000 / bpm, { immediate: true });
 updateMetronome();
+
+soundSelector01.addEventListener("click", () => {
+  selectedSound = 1;
+  soundSelector01.classList.add("selected");
+  soundSelector02.classList.remove("selected");
+});
+
+soundSelector02.addEventListener("click", () => {
+  selectedSound = 2;
+  soundSelector02.classList.add("selected");
+  soundSelector01.classList.remove("selected");
+});
+
+tapTempoButton.addEventListener("click", () => {
+  let currentTime = Date.now();
+  let timeBetweenTaps = currentTime - timeSinceLastTap;
+  timeSinceLastTap = currentTime;
+  bpm = Math.floor(60000 / timeBetweenTaps);
+  validateTempo();
+  updateMetronome();
+});
 
 decreaseTempoButton.addEventListener("click", () => {
   bpm--;
@@ -100,12 +129,15 @@ function playClick() {
   if (count === beatsPerMeasure) {
     count = 0;
   }
-  if (count === 0) {
-    clickLower.play();
-    clickLower.currentTime = 0;
-  } else {
-    clickUpper.play();
-    clickUpper.currentTime = 0;
-  }
+  // if (count === 0) {
+  //   clickLower.play();
+  //   clickLower.currentTime = 0;
+  // } else {
+  //   clickUpper.play();
+  //   clickUpper.currentTime = 0;
+  // }
+
+  clicks[selectedSound - 1].play();
+  clicks[selectedSound - 1].currentTime = 0;
   count++;
 }
